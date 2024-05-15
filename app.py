dabf71714a4ed2e42d2b797b480e5eb5f4b35858
rosscript <rosscript@proton.me>
2024-05-15 10:31:10 +0200

    chore: Refactor message handling logic and improve file saving

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
 client = TelegramClient('none', api_id, api_hash)
 
-# Gestore dei nuovi messaggi
 @client.on(events.NewMessage)
 async def handler(event):
     try:
@@ -26,12 +25,11 @@ async def handler(event):
         chat_id = event.chat_id
         sender = await event.get_sender()
         sender_id = sender.id
-        username = sender.username if sender.username else 'unknown'  # Gestione username mancante
+        username = sender.username if sender.username else 'unknown'
         chat_title, chat_type = await get_chat_name_and_type(event)
         folder_name = create_folder_name(chat_id, chat_title)
         base_folder = os.path.join('./chats', chat_type)
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        timestamp_for_file = datetime.now()
 
         print(f"Nuovo messaggio ricevuto da {username} in chat {chat_title} (ID: {chat_id}, Tipo: {chat_type})")
 
@@ -40,25 +38,25 @@ async def handler(event):
             media_type, file_name = get_media_type_and_filename(event.message.media)
             if file_name:  # Se il nome del file è valido
                 file_path = await event.message.download_media(file=f'./chats/{chat_type}/{folder_name}/{media_type}/{file_name}')
-                save_message(base_folder, folder_name, event.message.message, sender_id, username, f'{media_type.capitalize()} con testo', timestamp, file_path)
+                save_message(base_folder, folder_name, event.message.message, sender_id, username, f'{media_type.capitalize()} con testo', timestamp, chat_type, chat_title, file_path)
             else:
-                save_message(base_folder, folder_name, event.message.message, sender_id, username, 'text', timestamp)
+                save_message(base_folder, folder_name, event.message.message, sender_id, username, 'text', timestamp, chat_type, chat_title)
         
         # Gestione solo messaggio di testo
         elif event.message.message:
-            save_message(base_folder, folder_name, event.message.message, sender_id, username, 'text', timestamp)
+            save_message(base_folder, folder_name, event.message.message, sender_id, username, 'text', timestamp, chat_type, chat_title)
         
         # Gestione solo media
         elif event.message.media:
             media_type, file_name = get_media_type_and_filename(event.message.media)
             if file_name:  # Se il nome del file è valido
                 file_path = await event.message.download_media(file=f'./chats/{chat_type}/{folder_name}/{media_type}/{file_name}')
-                save_message(base_folder, folder_name, media_type.capitalize(), sender_id, username, media_type, timestamp, file_path)
+                save_message(base_folder, folder_name, media_type.capitalize(), sender_id, username, media_type, timestamp, chat_type, chat_title, file_path)
             else:
-                save_message(base_folder, folder_name, 'Unknown Media', sender_id, username, 'unknown', timestamp)
+                save_message(base_folder, folder_name, 'Unknown Media', sender_id, username, 'unknown', timestamp, chat_type, chat_title)
         
         else:
-            save_message(base_folder, folder_name, 'Unknown Media', sender_id, username, 'unknown', timestamp)
+            save_message(base_folder, folder_name, 'Unknown Media', sender_id, username, 'unknown', timestamp, chat_type, chat_title)
     
     except Exception as e:
         with open('error.log', 'a') as f:
--- utils.py
+++ utils.py
@@ -7,25 +7,20 @@ import re
 from datetime import datetime
 from telethon.tl.types import MessageMediaDocument, MessageMediaPhoto, PeerChannel, PeerChat, PeerUser
 
-# Funzione per creare il nome della cartella
 def create_folder_name(chat_id, chat_title):
     sanitized_title = re.sub(r'\s+', '', chat_title)  # Rimuovi spazi
     folder_name = f'[{chat_id}]_{sanitized_title}'
     return folder_name
 
-# Funzione per salvare il messaggio nel formato CSV
-def save_message_csv(csv_file_path, message_data):
+def save_message_csv(csv_file_path, message_data, include_header=False):
     file_exists = os.path.isfile(csv_file_path)
     with open(csv_file_path, 'a', newline='', encoding='utf-8') as csvfile:
-        fieldnames = ['sender_id', 'username', 'message_type', 'message', 'timestamp', 'media_path']
-        if file_exists:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
-        else:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        fieldnames = ['sender_id', 'username', 'message_type', 'message', 'timestamp', 'media_path', 'chat_type', 'chat_name']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
+        if not file_exists:
             writer.writeheader()
         writer.writerow(message_data)
 
-# Funzione per salvare il messaggio nel formato JSON
 def save_message_json(json_file_path, message_data):
     messages = []
     if os.path.exists(json_file_path):
@@ -35,32 +30,34 @@ def save_message_json(json_file_path, message_data):
     with open(json_file_path, 'w', encoding='utf-8') as jsonfile:
         json.dump(messages, jsonfile, ensure_ascii=False, indent=4)
 
-# Funzione per salvare il messaggio
-def save_message(base_folder, folder_name, message, sender_id, username, message_type, timestamp, media_path=None):
+def save_message(base_folder, folder_name, message, sender_id, username, message_type, timestamp, chat_type, chat_name, media_path=None):
     folder_path = os.path.join(base_folder, folder_name)
     os.makedirs(folder_path, exist_ok=True)
     
     csv_file_path = os.path.join(folder_path, 'messages.csv')
     json_file_path = os.path.join(folder_path, 'messages.json')
+    global_csv_file_path = os.path.join('./chats', 'messages.csv')
+    global_json_file_path = os.path.join('./chats', 'messages.json')
 
     message_data = {
         'sender_id': sender_id,
         'username': username or 'unknown',
         'message_type': message_type,
-        'message': message.replace('\n', '\\n'),  # Gestione dei messaggi multilinea
+        'message': message.replace('\n', '\\n'),
         'timestamp': timestamp,
-        'media_path': media_path
+        'media_path': media_path,
+        'chat_type': chat_type,
+        'chat_name': chat_name  # Aggiunta del nome della chat
     }
 
     save_message_csv(csv_file_path, message_data)
     save_message_json(json_file_path, message_data)
-    
-    print(f"Salvato messaggio in {folder_name} - Tipo: {message_type}, Mittente: {username}, Timestamp: {timestamp}")
+    save_message_csv(global_csv_file_path, message_data)
+    save_message_json(global_json_file_path, message_data)
 
-# Funzione per determinare il tipo di media e l'estensione del file
 def get_media_type_and_filename(media):
     if isinstance(media, MessageMediaPhoto):
-        return 'photo', f"{datetime.now().strftime('%d%m%Y%H%M%S')}.jpg"  # Le foto di Telegram sono generalmente jpg
+        return 'photo', f"{datetime.now().strftime('%d%m%Y%H%M%S')}.jpg"
     elif isinstance(media, MessageMediaDocument):
         mime_type = media.document.mime_type
         if media.document.attributes:
@@ -76,7 +73,6 @@ def get_media_type_and_filename(media):
     else:
         return 'unknown', None
 
-# Funzione per ottenere il nome e il tipo della chat
 async def get_chat_name_and_type(event):
     chat = await event.get_chat()
     if event.is_private:
@@ -85,7 +81,6 @@ async def get_chat_name_and_type(event):
     else:
         chat_name = chat.title
         if isinstance(event.message.to_id, PeerChannel):
-            # Verifica se è un canale
             if chat.megagroup:
                 chat_type = 'Groups'
             else:
@@ -96,6 +91,5 @@ async def get_chat_name_and_type(event):
             chat_type = 'Unknown'
     return chat_name, chat_type
 
-# Funzione per creare un nome file basato sul timestamp
 def create_file_name(timestamp, extension):
     return f"{timestamp.strftime('%d%m%Y%H%M%S')}.{extension}"
